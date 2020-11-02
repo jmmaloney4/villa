b4da369201f81af1c3ccb2730eda611ba5f402b8
Jack Maloney <jmmaloney4@gmail.com>
2020-11-02 17:43:36 -0600

    Add Cobra

---
+++ go.mod
@@ -0,0 +1,5 @@
+module githib.com/jmmaloney4/villa
+
+go 1.15
+
+require github.com/spf13/cobra v1.1.1
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/jmmaloney4/villa/cmd"
+
+func main() {
+	cmd.Execute()
+}
